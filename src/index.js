38fc457a271f51122b363536d89de964e5041daf
rodrigoarcanjo23 <rodrigoarcanjo690@gmail.com>
2022-10-20 19:07:23 -0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
-import NotFound from './pages/NotFound';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Desafio master blaster na descrição
+function Pagina404() {
+  return <div>Página 404</div>;
+}
 
+ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} exact />
-      <Route path="/cadastro/video" element={<CadastroVideo />} exact />
-      <Route path="/cadastro/categoria" element={<CadastroCategoria />} exact />
-      <Route path="/404" element={<NotFound />} />
-
+      <Route path="/cadastro/video" element={<CadastroVideo />} />
+      <Route path="/cadastro/categoria" element={<CadastroCategoria />} />
+      <Route element={<Pagina404 />} />
     </Routes>
   </BrowserRouter>,
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+  document.getElementById('root'),
 );
